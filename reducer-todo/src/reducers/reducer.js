b06af0e45ae1f4dbc03440ca1a29b3680087e4a2
Roland J Jarvis <Rjarvis87@gmail.com>
2019-09-21 15:17:06 -0400

    switched reducer

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -6,14 +6,44 @@ const initialState =
         completed: false,
         id:  Date().getTime()
     }
-const reducer = (state) => {
-    return 
-    { 
-        item: state.item;
-        completed: state.completed;
-        id: state.Date().getTime();
-    };
+const reducer = (state, action) => {
+    switch(action.type)
+    {
+        case 'addTask':
+            return
+            {
+                item: action.payload;
+                completed: false;
+                id: Date().getTime();
+            };
+        case 'taskComplete':
+            return 
+            {
+                item: state.item;
+                completed: action.payload;
+                id: state.id;
+            };
+        default: return state;
+    }
+    /*
+    if(action.type === 'addTask') {
+        return 
+        { 
+            item: action.payload;
+            completed: false;
+            id: Date().getTime();
+        };
+    }
+
+    if(action.type === 'taskComplete'){
+        return 
+        {
+            item: state.item;
+            completed: action.payload;
+            id: state.id
+        };
+    }*/
 }
 
 
-export default {reducer, initialState};
+export default reducer(initialState);
